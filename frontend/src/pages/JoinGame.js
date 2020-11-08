8fd3d4df950389d3dd8710ef54536cd7fc15ecf9
Frances <francesannlee7@gmail.com>
2020-11-08 22:59:07 +1100

    join game done

--- frontend/src/pages/JoinGame.js
+++ frontend/src/pages/JoinGame.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import { TextField, Typography, Button } from '@material-ui/core';
 import NavBar from '../UIComponents/NavBar';
+import API from '../utils/api';
+
+const api = new API('http://localhost:5005');
+
+// game id is 984485397, session is 191470
 
 function JoinGame() {
   const [joinid, setJoinID] = React.useState('');
   const [nickname, setNickname] = React.useState('');
 
   const join = async () => {
-
+    console.log(nickname);
+    const path = 'play/join/191470'; // will need to change path later so it has id of session
+    const options = {
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify({
+        name: nickname,
+      }),
+    };
+    const res = await api.post(path, options);
+    console.log(res);
   };
 
   const handleClick = (e) => {
